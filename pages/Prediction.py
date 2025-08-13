c6b78e3c543a88b3d0c5a078bb2482dd84565fdf
Ahmed <gonimeahmed73@gmail.com>
2025-08-13 20:10:49 +0300

    Add files via upload

---
+++ pages/Prediction.py
@@ -0,0 +1,64 @@
+#importing the file
+import streamlit as st
+import joblib as jb
+import pandas as pd
+
+#identify the models
+model = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\results\model_train_result.sav')
+LE = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\results\Label_encoding_sex.sav')
+OE = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\results\One_Hot_encoding_embarked.sav')
+scalar = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\results\Standared_Scalar.sav')
+column_after_encoding = jb.load(r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\results\column_encoded_result.sav')
+
+#adding a title and subtitle
+st.set_page_config(page_title='Prediction', page_icon='🔍')
+st.title("🔍 Titanic Survival Prediction")
+st.info("This program predicts if a person survived or not based on input data.")
+
+#identify our inputs
+Pclass = st.number_input('Pclass', min_value=1, max_value=3, step=1, format='%d')
+Sex = st.selectbox('Gender', ['male', 'female'])
+Age = st.slider('Age', min_value=1, max_value=100)
+SibSp = st.number_input('SibSp', step=1)
+Parch = st.number_input('Parch', step=1)
+Fare = st.number_input('Fare', min_value=0.0, max_value=500.0, step=0.1, format='%.2f')
+Embarked = st.selectbox('Embarked', ['C', 'Q', 'S'])
+
+#convert them to a dataframe
+df = pd.DataFrame({
+    'Pclass' : [Pclass],
+    'Sex' : [Sex],
+    'Age' : [Age],
+    'SibSp' : [SibSp],
+    'Parch' : [Parch],
+    'Fare' : [Fare],
+    'Embarked' : [Embarked]
+})
+#making a button for start operation and prediction
+con = st.button('Start predicting')
+if con:
+    #label encoding
+    df['Sex'] = LE.transform(df['Sex'])
+    #one-hot encoding
+    embarked_encoded = pd.DataFrame(OE.transform(df[['Embarked']]),
+                                    columns = OE.get_feature_names_out(['Embarked']))
+    #concatenate data and sort it as the train time
+    df = pd.concat([df.drop('Embarked', axis=1), embarked_encoded], axis=1)
+    df = df.reindex(columns=column_after_encoding)
+    #Standariz our data
+    df_scaled = scalar.transform(df)
+    #get the predict
+    prediction = model.predict(df_scaled)[0]
+    probability = round(float(model.predict_proba(df_scaled)[0][prediction]), 3)
+    #making the output in a nice way
+    tap1, tap2 = st.tabs(['Prediction 💡', 'Details 📊'])
+    with tap1:
+        if prediction == 1:
+            st.success('The person survived ✅')
+        else :
+            st.error('The preson did not survive  💀')
+    with tap2:
+        st.info(f'The Prediction Result is : {prediction}')
+        st.success(f'📌 The probability of survival is : {probability}')
+        st.error(f'📌 The probability of not survival is : {probability}')
+        
---
+++ pages/about.py
@@ -0,0 +1,55 @@
+import streamlit as st
+#Page name and icon
+st.set_page_config(page_title='About', page_icon='📜')
+#some information about the project
+st.title('About Page')
+st.header('About this Project 📜')
+st.write('The data source came from Kaggle website')
+st.write('The used model is K Nearest Neighbors (KNN) classifier one with Default setting')
+st.write('The Libraries used in this project are:')
+st.write('1 - pandas for reading data')
+st.write('2 - sklearn for preprocessing data and building model')
+st.write('3 - joblib to save and load the model')
+st.write('4 - streamlit for building the web app interface (As workframe)')
+
+#some information about the creator
+st.header('About the Creator 👨‍💻')
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.image(
+        r'C:\I will prepare my self to destroy the world\Programming\python program\MY Projects\Classification Projects\1 - Titanik Survive Prediction app\FB_IMG_1726001609431.jpg',
+        width=150,
+        caption='Ahmed Ghoneim'
+    )
+with col2:
+    st.write('**Name :** Ahmed Ghoneim')
+    st.write('**E-mail :** ahmedghoneim658@gmail.com')
+    #social media links
+    st.markdown(
+        """
+        <style>
+            .social-icons {
+                display: flex;
+                gap: 15px;
+                margin-top: 20px;
+            }
+            .social-icons img {
+                width: 40px;
+                height: 40px;
+            }
+        </style>
+        
+        <div class="social-icons">
+            <a href="https://www.facebook.com/Ahmed Ghoniem" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook"/>
+            </a>
+            <a href="https://www.instagram.com/Ahmed Ghoniem" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/733/733558.png" alt="Instagram"/>
+            </a>
+            <a href="https://www.linkedin.com/in/Ahmed Ghoneim/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn"/>
+            </a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
